42047e180765786beb0ce9655cafd550bb0999af
Erik Rodenburgh <erikrodenburgh@gmail.com>
2024-07-02 13:03:02 -0400

    Defined the C -> F and C->K functions

---
+++ temp_converter/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+double CelsiusToFahrenheit(double celsius){
+    return (celsius * 9/5.f) + 32;
+}
+
+double CelsiusToKelvin(double celsius){
+    return celsius + 273.15;
+}
+
+double FahrenheitToCelsius(){
+
+}
+
+double FahrenheitToKelvin() {
+
+}
+
+double KelvinToCelsius(){
+
+}
+
+double KelvinToFahrenheit(){
+
+}
+
+int main(){
+    
+    
+    return 0;
+}
