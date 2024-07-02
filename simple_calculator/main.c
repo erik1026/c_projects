66ea24a6d4f1fe4f1e509ecb5e79f140effd5a41
Erik Rodenburgh <erikrodenburgh@gmail.com>
2024-07-02 12:21:39 -0400

    Added a loop that listens for user input and performs the operation

--- simple_calculator/main.c
+++ simple_calculator/main.c
@@ -30,5 +30,40 @@ double division(double op1, double op2){
 
 int main(){
 
+    // This will be used as my flag
+    int exit = 0;
+    char operation;
+    double operand1, operand2;
+
+
+    while(!exit){
+        printf("Please enter a math operations from the following: +, -, *, /, e(to exit)\n");
+        scanf(" %c", &operation); // space was added to prevent a newline character from being read in from the input buffer
+        printf("Please enter two operands separated by a comma:\n");
+        scanf("%lf, %lf", &operand1, &operand2);
+        switch (operation)
+        {
+        case '+':
+            printf("Result of addition: %lf\n", addition(operand1, operand2));
+            break;
+        case '-':
+            printf("Result of subtraction: %lf\n", substraction(operand1, operand2));
+            break;
+        case '*':
+            printf("Result of multiplication: %lf\n", multiplication(operand1, operand2));
+            break;
+        case '/':
+            printf("Result of division: %lf\n", division(operand1, operand2));
+            break;
+        case 'e':
+            printf("exiting...\n");
+            exit=1;
+            break;
+        default:
+            printf("Unknown operation entered...\n");
+            break;
+        }
+    }
+
     return 0;
 }
