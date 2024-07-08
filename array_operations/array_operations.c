4c06f03616f92f42b7e0718ff9b5980212fc58b5
Erik Rodenburgh <erikrodenburgh@gmail.com>
2024-07-08 14:41:11 -0400

    Created the array_max function that determines the max value of an integer array

--- array_operations/array_operations.c
+++ array_operations/array_operations.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <limits.h>
+
+int array_max(int* array, int size){
+
+    int max = INT_MIN;
+    for(int i = 0; i < size; ++i){
+        if(max < array[i]){
+            max = array[i];
+        }
+    }
+
+    return max;
+}
 
 int main() {
     
+    int myArray[] = {10, 11, 50, 36, -1};
+    int myArraySize = sizeof(myArray) / sizeof(int);
+    
+    // Array max
+    printf("Maximum of my array: %d\n", array_max(myArray, myArraySize));
+
     return 0;
 }
