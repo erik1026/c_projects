d15821da5e69ef0cadfcac1bd28719fd365f3507
Erik Rodenburgh <erikrodenburgh@gmail.com>
2024-07-08 14:46:23 -0400

    Created the array_min function that returns the minimum of the array

--- array_operations/array_operations.c
+++ array_operations/array_operations.c
@@ -13,6 +13,18 @@ int array_max(int* array, int size){
     return max;
 }
 
+int array_min(int* array, int size){
+    int min = INT_MAX;
+
+    for(int i = 0; i < size; ++i){
+        if (min > array[i]){
+            min = array[i];
+        }
+    }
+
+    return min;
+}
+
 int main() {
     
     int myArray[] = {10, 11, 50, 36, -1};
@@ -20,6 +32,9 @@ int main() {
     
     // Array max
     printf("Maximum of my array: %d\n", array_max(myArray, myArraySize));
+
+    // Array min
+    printf("Minimum of my array: %d\n", array_min(myArray, myArraySize));
 
     return 0;
 }
